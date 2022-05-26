45413114ba95fb117eab62b9d721e1ce4d6083e8
Ansel Chang <ansel.chang@yahoo.com>
2022-05-26 17:58:01 -0400

    drawing

--- include/Utility/VisualField.cpp
+++ include/Utility/VisualField.cpp
@@ -6,10 +6,24 @@ int VisualField::inchesToPixels(float inches) {
   return inches * (204.0 / 144.0);
 }
 
-int VisualField::getPixel(float inches) {
+// in this case, (0,0) in inches is the bottom left corner of hte field
+int VisualField::getX(float inches, bool isTileUnits) {
+  if (isTileUnits) inches *= 24;
   return MARGIN + inchesToPixels(inches);
 }
 
+int VisualField::getY(float inches, bool isTileUnits) {
+  if (isTileUnits) inches *= 24;
+  return MARGIN + 204 - inchesToPixels(inches);
+}
+
+void VisualField::drawLine(float x1, float y1, float x2, float y2, bool isTileUnits) {
+  Brain.Screen.drawLine(getX(x1, isTileUnits), getY(y1, isTileUnits), getX(x2, isTileUnits), getY(y2, isTileUnits));
+}
+
+color vexblue(37, 75, 143);
+color vexred(204, 57, 58);
+color vexyellowline(225, 167, 102);
 void VisualField::drawField() {
 
   //field from (20,20) to (224, 224)
@@ -21,4 +35,20 @@ void VisualField::drawField() {
     Brain.Screen.drawLine(MARGIN, i, MARGIN + 204, i); // draw row
     Brain.Screen.drawLine(i, MARGIN, i, MARGIN + 204); // draw col
   }
+
+  // draw bumpers
+  Brain.Screen.setPenWidth(4);
+  Brain.Screen.setPenColor(vexblue);
+  drawLine(1, 2, 2, 2, true);
+  drawLine(2, 2, 2, 1, true);
+  Brain.Screen.setPenColor(vexred);
+  drawLine(4, 5, 4, 4, true);
+  drawLine(4, 4, 5, 4, true);
+
+  // draw mid line
+  Brain.Screen.setPenColor(vexyellowline);
+  Brain.Screen.setPenWidth(5);
+  drawLine(0, 6, 6, 0, true);
+
+
 }
--- include/Utility/VisualField.h
+++ include/Utility/VisualField.h
@@ -10,6 +10,8 @@ private:
   const int MARGIN = 20;
 
   int inchesToPixels(float inches);
-  int getPixel(float inches);
+  int getX(float inches, bool isTileUnits = false);
+  int getY(float inches, bool isTileUnits = false);
+  void drawLine(float x1, float y1, float x2, float y2, bool isTileUnits = false);
 
 };
